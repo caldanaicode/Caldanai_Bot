2de2d7541f92c8cf99b6461a7df14503c9bdf293
caldanaicode <>
2021-04-22 21:56:28 +0000

    Reverted parser to working order since the initial commit was broken.

--- Caldanai/lib/rpg/parser.py
+++ Caldanai/lib/rpg/parser.py
@@ -3,16 +3,16 @@ from random import choice
 
 class Parser:
 	optionsRE = r'\{(?P<options>[^{}]+)\}'
+	nounRE = r'\$n'
+	commandRE = r'\$c'
 	tokenRE = r'\$(?P<token>\w+)'
 
 	@staticmethod
 	def parse(input: str, name: str = None, command: str = None) -> str:
 		output = input
+		
 		for match in re.finditer(Parser.optionsRE, output):
 			output = re.sub(Parser.optionsRE, choice(match.group('options').split('|')), output, 1)
-		
-		for match in re.finditer(Parser.tokenRE, output):
-			
 		
 		if name is not None:
 			output = re.sub(Parser.nounRE, name, output)
