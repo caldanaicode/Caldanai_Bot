85381c39c01872712bca0f47130943396215d741
Caels <jessiekblair@gmail.com>
2021-04-29 17:23:06 -0700

    Fixed weight sum errors, hopefully, using math.fsum.

--- Caldanai/lib/rpg/inventory/inventory.py
+++ Caldanai/lib/rpg/inventory/inventory.py
@@ -2,11 +2,11 @@ from .item import Item
 from .weapon import Weapon
 from ....db.db import MongoDB
 from bson.objectid import ObjectId
+from math import fsum
 
 
 class Inventory:
     def __init__(self, contents: list = ()):
-        self.__weight = 0
         self.__contents: dict = {}
         for item in contents:
             self.add(item)
@@ -23,19 +23,17 @@ class Inventory:
     def add(self, item) -> bool:
         item.save()
         self.__contents[str(item.id)] = item
-        self.__weight += item.weight
         return True
 
     # Remove an item from the inventory if it exists.
     def remove(self, item):
         del self.__contents[str(item.id)]
-        self.__weight -= item.weight
         MongoDB.items.delete_one({'_id': item.id})
         return True
 
     # Returns the current inventory weight
     def get_weight(self):
-        return self.__weight
+        return fsum([i.weight for i in self.__contents.values()])
 
     # Returns an index and item from the inventory.
     def enumeration(self):
