e9c1b46d1d48198ce0dad7b784a367706ec4e79b
Caels <jessiekblair@gmail.com>
2021-05-09 11:11:06 -0700

    Converted dice to a class with static methods.

--- Caldanai/lib/rpg/dice.py
+++ Caldanai/lib/rpg/dice.py
@@ -1,13 +1,44 @@
-from random import randint
+from random import randint, choice
 
 
-def quick_roll(ndn: str) -> int:
-	try:
-		count, sides = map(int, ndn.lower().split('d'))
-	except Exception as e:
-		print(e)
-	
-	result = 0
-	for _ in range(count):
-		result += randint(1, sides)
-	return result
+class Dice:
+	@staticmethod
+	def quick_roll(ndn: str) -> int:
+		count = sides = 0
+		try:
+			count, sides = map(int, ndn.lower().split('d'))
+		except Exception as e:
+			print(e)
+
+		result = 0
+		for _ in range(count):
+			result += randint(1, sides)
+		return result
+
+	@staticmethod
+	def coin_toss() -> str:
+		return choice(["heads", "tails"])
+
+	@staticmethod
+	def d4() -> int:
+		return randint(1, 4)
+
+	@staticmethod
+	def d6() -> int:
+		return randint(1, 6)
+
+	@staticmethod
+	def d8() -> int:
+		return randint(1, 8)
+
+	@staticmethod
+	def d10() -> int:
+		return randint(1, 10)
+
+	@staticmethod
+	def d12() -> int:
+		return randint(1, 12)
+
+	@staticmethod
+	def d20() -> int:
+		return randint(1, 20)
