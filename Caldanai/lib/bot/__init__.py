6ad4ea41f31763e377ccb1224167e70db7dbd1ed
Caels <jessiekblair@gmail.com>
2022-02-16 13:51:02 -0700

    Capitalization change, and collapsed lines.

--- Caldanai/lib/bot/__init__.py
+++ Caldanai/lib/bot/__init__.py
@@ -103,13 +103,11 @@ class Bot(BotBase):
 	async def get_forbidden_response(ctx: Context) -> str:
 		return choice([
 			f"I'm afraid I can't do that, {ctx.author.mention}.",
-			f"Perhaps, one day, you shall hold that kind of power over me, {ctx.author.mention}. But today is not "
-			f"that day.",
+			f"Perhaps, one day, you shall hold that kind of power over me, {ctx.author.mention}. But today is not that day.",
 			f"You're not the boss of me, {ctx.author.mention}! Just who do you think you are?!",
 			f"Unable to comply, {ctx.author.mention}, please elevate status and try again.",
-			"```\nOne of these days\nI'm gonna love me.\nOne of these days\nI'll rise above me.\nOne of these "
-			"days...```",
-			"We're sorry. the number you have dialed is no longer in service. Please hang up, and try your call again."
+			"```\nOne of these days\nI'm gonna love me.\nOne of these days\nI'll rise above me.\nOne of these days...```",
+			"We're sorry. The number you have dialed is no longer in service. Please hang up, and try your call again."
 		])
 
 	async def on_command_error(self, ctx, exc):
