666aa8b19f05d352bd02c3c8c33de1fb70d8dd5d
Caels <jessiekblair@gmail.com>
2022-04-15 22:16:35 -0700

    Defaulted dice count to 1 when no number precedes an NdN format.

--- Caldanai/lib/rpg/helpers/dice.py
+++ Caldanai/lib/rpg/helpers/dice.py
@@ -62,6 +62,13 @@ class Dice:
 	def get_ndn(self):
 		return f"{self.count}d{self.sides}"
 
+	def __int__(self, s: str):
+		try:
+			return 1 if s == '' else int(s)
+		except Exception as e:
+			stdout(e)
+			return 0
+
 	@classmethod
 	def from_ndn(cls, ndn: str) -> Union["Dice", None]:
 		"""
@@ -70,11 +77,7 @@ class Dice:
 		:param ndn: The number of dice and the sides per dice, such as "1d6" or "2d10"
 		:return: The Dice instance created.
 		"""
-		try:
-			count, sides = map(int, ndn.lower().split('d'))
-		except Exception as e:
-			stdout(e)
-			return None
+		count, sides = map(cls.__int__, ndn.lower().split('d'))
 
 		if count < 1 or sides < 2:
 			return None
