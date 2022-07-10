b01fe2fda0eacbe90e7c2417066da5277fbc62eb
Caels <jessiekblair@gmail.com>
2022-07-10 13:23:50 -0700

    Added SURVIVE and RANDOM AgressionLevels

--- Caldanai/lib/rpg/helpers/enums.py
+++ Caldanai/lib/rpg/helpers/enums.py
@@ -5,6 +5,8 @@ class AggressionLevels(Enum):
 	PASSIVE = 0			# Never attacks
 	VENGEFUL = 1		# Hit & run
 	RAMPAGE = 2			# Attacks until defeated or left alone
+	SURVIVE = 3			# Attacks until low health, then flees
+	RANDOM = 4			# Might flee or attack with every turn
 
 
 class CombatRanges(IntFlag):
