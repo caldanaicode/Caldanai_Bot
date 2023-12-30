d56c2655749f44f56a891ebb388de87e7473856f
caldanaicode <jessiekblair@gmail.com>
2023-12-30 01:34:37 -0700

    Converted DB class to static

--- Caldanai/db/__init__.py
+++ Caldanai/db/__init__.py
@@ -13,20 +13,6 @@ from Caldanai import Observer, Subject
 from Caldanai.environment import DB_CONNECTION
 
 
-class DatabaseConnectionError(Exception):
-	"""Represents a database connection error."""
-	pass
-
-
-class DatabaseMessages(IntEnum):
-	"""Contains message values for database observer notification."""
-
-	# Connection states
-	CONNECTION_SUCCESS = 0
-	CONNECTION_RETRY = 1
-	CONNECTION_ERROR = 2
-
-
 class DatabaseCollections(Enum):
 	AUTH = 'auth'
 	SERVERS = 'servers'
@@ -40,78 +26,79 @@ class DatabaseCollections(Enum):
 		return value in cls.__members__ and value != cls.AUTH.name
 
 
-class MongoDatabase(Observer, Subject):
-	"""Wrapper for MongoDB operations."""
-
-	class DoubleBuffer:
+class DoubleBuffer:
 		"""A double buffer class for queue processing."""
 		def __init__(self):
-			self.q1 = Queue()
-			self.q2 = Queue()
-			self.retry = Queue()
-			self.active = self.q1
-			self.passive = self.q2
+			DB.q1 = Queue()
+			DB.q2 = Queue()
+			DB.retry = Queue()
+			DB.active = DB.q1
+			DB.passive = DB.q2
 		
-		def put(self, item):
+		def put(item):
 			"""Puts an item into the active queue."""
-			self.active.put(item)
+			DB.active.put(item)
 		
 		def swap(self):
 			"""Swaps the buffer queues."""
-			self.active, self.passive = self.passive, self.active
+			DB.active, DB.passive = DB.passive, DB.active
 		
 		def get_all(self):
 			"""Retrieves all items from the active buffer and clears it."""
-			self.swap()
-			items = list(self.passive.queue)
-			self.passive.queue.clear()
+			DB.swap()
+			items = list(DB.passive.queue)
+			DB.passive.queue.clear()
 			return items
+
+
+class DB(Observer, Subject):
+	"""Wrapper for MongoDB operations."""
+	_mongoClient: MongoClient = MongoClient(DB_CONNECTION)
+	_mongoDB: Database = _mongoClient.caldanaiDB
+	_queues = defaultdict(DoubleBuffer)
 	
-	def __init__(self):
-		"""Initializes the database connection."""
-		self._mongoClient: MongoClient = MongoClient(DB_CONNECTION)
-		self._mongoDB: Database = self._mongoClient.caldanaiDB
-		self._queues = defaultdict(self.DoubleBuffer)
-	
-	@property
-	def is_connected(self) -> bool:
+	@staticmethod
+	def is_connected() -> bool:
 		"""Pings the MongoDB client to verify connectivity."""
 		try:
-			self._mongoClient.admin.command('ping')
+			DB._mongoClient.admin.command('ping')
 			return True
 		except ConnectionFailure:
 			return False
 	
-	async def update(self, message):
+	@staticmethod
+	async def update(message):
 		if isinstance(message, dict):
 			for k,v in message.items():
 				if isinstance(k, DatabaseCollections) and DatabaseCollections.is_writable(k):
-					self._queues[k.value].put(v)
+					DB._queues[k.value].put(v)
 	
+	@staticmethod
 	def check_connection(func):
 		"""Verifies database connectivity, raising a DatabaseConnectionError if the connection fails."""
 		@functools.wraps(func)
-		def wrapper(self, *args, **kwargs):
-			if self.is_connected:
-				return func(self, *args, **kwargs)
+		def wrapper(*args, **kwargs):
+			if DB.is_connected:
+				return func(*args, **kwargs)
 			else:
 				logging.error(f"Error occurred while performing connection test for {func}.")
 		return wrapper
 
 	@check_connection
-	def close_db_connection(self):
+	@staticmethod
+	def close_db_connection():
 		"""Closes the database connection."""
-		self._mongoClient.close()
+		DB._mongoClient.close()
 
 	@tasks.loop(minutes=1)
 	async def batch_write(self):
 		"""Performs batch writing to the database for the queued items."""
-		collections = list(self._queues.keys())
+		collections = list(DB._queues.keys())
 		for collection in collections:
-			if self.is_connected:
-				ops = self._queues[collection].get_all()
+			if DB.is_connected():
+				ops = DB._queues[collection].get_all()
 				try:
-					self._mongoDB[collection].bulk_write(ops, ordered=False)
+					DB._mongoDB[collection].bulk_write(ops, ordered=False)
 				except BulkWriteError as e:
 					logging.error(f"Error occurred while performing bulk write operation: {e.details}")
 					raise e
@@ -119,99 +106,116 @@ class MongoDatabase(Observer, Subject):
 				logging.error("No connection for batch_write operation.")
 
 	@check_connection
-	def find_players_by_user_id(self, user_id):
+	@staticmethod
+	def find_players_by_user_id(user_id):
 		"""Finds all players associated with the supplied user ID."""
-		return self._mongoDB.players.find({'user_id': user_id})
+		return DB._mongoDB.players.find({'user_id': user_id})
 	
 	@check_connection
-	def get_player(self, guild_id, user_id):
+	@staticmethod
+	def get_player(guild_id, user_id):
 		"""Returns the player associated with a guild ID and user ID."""
-		return self._mongoDB.players.find_one(self, {'guild_id': guild_id, 'user_id': user_id})
+		return DB._mongoDB.players.find_one({'guild_id': guild_id, 'user_id': user_id})
 	
 	@check_connection
-	def find_all_games(self):
+	@staticmethod
+	def find_all_games():
 		"""Find all games in the database."""
-		return self._mongoDB.games.find()
+		return DB._mongoDB.games.find()
 	
 	@check_connection
-	def get_auth(self):
+	@staticmethod
+	def get_auth():
 		"""Returns the authorization record."""
-		return self._mongoDB.auth.find_one()
+		return DB._mongoDB.auth.find_one()
 	
 	@check_connection
-	def get_server_by_guild_id(self, id):
+	@staticmethod
+	def get_server_by_guild_id(id):
 		"""Returns the server associated with the given Guild ID."""
-		return self._mongoDB.servers.find_one({"guild_id": id})
+		return DB._mongoDB.servers.find_one({"guild_id": id})
 
-	def delete_game(self, guild_id):
+	@staticmethod
+	def delete_game(guild_id):
 		"""Enqueues deletion of a game and its associated players from the database."""
-		self._queues["games"].put(DeleteOne({'guild_id': guild_id}))
-		self._queues["players"].put(DeleteMany({'guild_id': guild_id}))
+		DB._queues["games"].put(DeleteOne({'guild_id': guild_id}))
+		DB._queues["players"].put(DeleteMany({'guild_id': guild_id}))
 	
-	def update_game(self, guild_id, guild_dict, upsert=False):
+	@staticmethod
+	def update_game(guild_id, guild_dict, upsert=False):
 		"""Enqueues an update for a game object in the database."""
-		self._queues["games"].put(UpdateOne(
+		DB._queues["games"].put(UpdateOne(
 			{'guild_id': guild_id},
 			{'$set': guild_dict},
 			upsert=upsert))
 	
-	def update_statistic(self, guild_id, inc_doc):
+	@staticmethod
+	def update_statistic(guild_id, inc_doc):
 		"""Enqueues an update for the server statistics."""
-		self._queues["statics"].put(UpdateOne(
+		DB._queues["statics"].put(UpdateOne(
 			{'guild_id': guild_id},
 			{'$inc': inc_doc },
 			upsert=True))
 
-	def update_user_statics(self, doc):
+	@staticmethod
+	def update_user_statics(doc):
 		"""Enqueues an insertion for the given user statistics record."""
-		self._queues["user_command_statics"].put(InsertOne(doc))
+		DB._queues["user_command_statics"].put(InsertOne(doc))
 
-	def update_player(self, guild_id, user_id, player_dict):
+	@staticmethod
+	def update_player(guild_id, user_id, player_dict):
 		"""Enqueues a player upsert."""
-		self._queues["players"].put(UpdateOne(
+		DB._queues["players"].put(UpdateOne(
 			{"guild_id": guild_id, "user_id": user_id},
 			{"$set": player_dict},
 			upsert=True))
 
-	def insert_server(self, guild_id, name=None, prefix='$'):
+	@staticmethod
+	def insert_server(guild_id, name=None, prefix='$'):
 		"""Enqueues a server insert."""
-		self._queues["servers"].put(InsertOne({'guild_id': guild_id, 'name': name, 'prefix': prefix}))
+		DB._queues["servers"].put(InsertOne({'guild_id': guild_id, 'name': name, 'prefix': prefix}))
 	
-	def delete_server(self, guild_id):
+	@staticmethod
+	def delete_server(guild_id):
 		"""Enqueueus deletion of a server, and all associated games and players."""
-		self._queues["servers"].put(DeleteOne({'guild_id': guild_id}))
-		self._queues["games"].put(DeleteMany({'guild_id': guild_id}))
-		self._queues["players"].put(DeleteMany({'guild_id': guild_id}))
+		DB._queues["servers"].put(DeleteOne({'guild_id': guild_id}))
+		DB._queues["games"].put(DeleteMany({'guild_id': guild_id}))
+		DB._queues["players"].put(DeleteMany({'guild_id': guild_id}))
 	
 	@check_connection
-	def find_players_by_guild_id(self, guild_id):
+	@staticmethod
+	def find_players_by_guild_id(guild_id):
 		"""Retrieves all players associated with a guild ID."""
-		return self._mongoDB.players.find({'guild_id': guild_id})
+		return DB._mongoDB.players.find({'guild_id': guild_id})
 	
-	def delete_player(self, guild_id, user_id):
+	@staticmethod
+	def delete_player(guild_id, user_id):
 		"""Deletes a player from the database."""
-		self._queues["players"].put(DeleteOne({'guild_id': guild_id, 'user_id': user_id}))
+		DB._queues["players"].put(DeleteOne({'guild_id': guild_id, 'user_id': user_id}))
 	
-	def delete_all_players(self, guild_id):
+	@staticmethod
+	def delete_all_players(guild_id):
 		"""Deletes all players in the database associated with the given guild_id."""
-		self._queues["players"].put(DeleteMany({'guild_id': guild_id}))
+		DB._queues["players"].put(DeleteMany({'guild_id': guild_id}))
 	
-	def update_server_prefix(self, guild_id, prefix):
+	@staticmethod
+	def update_server_prefix(guild_id, prefix):
 		"""Sets the server's prefix."""
-		self._queues["servers"].put(UpdateOne({'guild_id': guild_id}, {'$set': {'prefix': prefix}}))
+		DB._queues["servers"].put(UpdateOne({'guild_id': guild_id}, {'$set': {'prefix': prefix}}))
 	
 	@check_connection
-	def get_game_by_guild_id(self, guild_id):
+	@staticmethod
+	def get_game_by_guild_id(guild_id):
 		"""Retrieves a game by guild ID."""
-		return self._mongoDB.games.find_one({'guild_id': guild_id})
+		return DB._mongoDB.games.find_one({'guild_id': guild_id})
 	
-	def insert_game(self, guild_id, channel_id):
+	@staticmethod
+	def insert_game(guild_id, channel_id):
 		"""Inserts a game into the database."""
-		self._queues["games"].put(InsertOne({'guild_id': guild_id, 'channelId': channel_id}))
+		DB._queues["games"].put(InsertOne({'guild_id': guild_id, 'channelId': channel_id}))
 	
 	@check_connection
-	def get_last_command(self, guild_id):
+	@staticmethod
+	def get_last_command(guild_id):
 		"""Retrieves the last command recorded in the database."""
-		return self._mongoDB.user_command_statics.find({'guild_id': guild_id}).sort('timestamp', -1).limit(1)
-
-DB = MongoDatabase()
+		return DB._mongoDB.user_command_statics.find({'guild_id': guild_id}).sort('timestamp', -1).limit(1)
